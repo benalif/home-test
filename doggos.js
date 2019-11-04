ca3baa70e394adf2b0d4fcd715b07591e06c625e
benalif <farid.benali89@gmail.com>
2019-11-04 19:58:44 +0100

    first commit

---
+++ doggos.js
@@ -0,0 +1,23 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");
+
+promise
+  .then(function(response) {
+    const processingPromise = response.json();
+    return processingPromise;
+  })
+  .then(function(processedResponse) {
+    const img = document.createElement("img");
+    img.src = processedResponse.message;
+    img.alt = "Cute doggo";
+    doggos.appendChild(img);
+  });
+
+  const test = fetch(DOG_URL);
+
+  test.then(function(response))
+  {
+
+  }
